b2aa48c2bb74591d55a4d7a2f487497d3e890ff5
public123 <govjmal@gmail.com>
2024-05-03 00:20:42 +1000

    better

--- src/features/taggable-pdf/region/top-panel/topPanel.tsx
+++ src/features/taggable-pdf/region/top-panel/topPanel.tsx
@@ -14,11 +14,13 @@ interface Props {
 
 export default function TopPanel({ userFriendlyName, location, colour, actionsComponent }: Props) {
   const { x, y, width, height } = location;
-  const panelHeight = 80;
+  const panelHeight = 34;
 
   return (
     <div
       style={{
+        display: "flex",
+        flexDirection: "row",
         top: -panelHeight,
         height: panelHeight,
         left: -1,
@@ -27,6 +29,7 @@ export default function TopPanel({ userFriendlyName, location, colour, actionsCo
         padding: "0px 4px",
         position: "absolute",
         width: width,
+        // minWidth: "160px",
         overflow: "hidden",
         userSelect: "none",
         wordBreak: "break-all",
@@ -36,9 +39,13 @@ export default function TopPanel({ userFriendlyName, location, colour, actionsCo
         borderTopRightRadius: "2px",
         zIndex: 1
       }}>
-      <div style={{ fontSize: "12px" }}>{userFriendlyName}</div>
-      <div style={{ fontSize: "8px" }}>{`X: ${x}, Y: ${y}, W: ${width}, H: ${height}`}</div>
-      {!!actionsComponent && <div className="p-2">{actionsComponent}</div>}
+      <div>
+        <div style={{ fontSize: "12px" }}>{userFriendlyName}</div>
+        <div style={{ fontSize: "8px" }}>{`X: ${x}, Y: ${y}, W: ${width}, H: ${height}`}</div>
+      </div>
+      {!!actionsComponent && (
+        <div style={{ paddingTop: "2px", position: "absolute", right: "2px" }}>{actionsComponent}</div>
+      )}
     </div>
   );
 }
